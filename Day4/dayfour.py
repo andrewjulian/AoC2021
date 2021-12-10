dbfaf306684e02735aaed042f99b2dd897e79004
+ <+>
2021-12-10 08:58:05 -0700

    Day 4 Planning Comments

---
+++ Day4/dayfour.py
@@ -0,0 +1,11 @@
+# store random values as a randomArray
+
+# store each table as a 2D arraya
+
+# evaluate each table for values in randomArray using [][] and nested for loops
+    # change value to 0 if found, otherwise leave value as is
+    # include test(s) to determine if a winning row or column is found
+
+# total the values remaining in teach 2D array (for each table)
+# multiply the board sum total and mulitply by winning number from randomArray
+
